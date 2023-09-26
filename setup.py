ddf36cb85233ec17b90c6d9faf88199c133c3f6a
Elías Martínez <mselias97@gmail.com>
2023-09-26 11:42:21 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import setuptools
+
 setuptools.setup(
     name="python_world",
     version="0.0.1",
@@ -5,9 +7,5 @@ setuptools.setup(
     author_email="mselias97@gmail.com",
     description="A function that returns 'world'",
     url="https://github.com/elmarsan/python_world_elmarsan",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ]
+    packages=setuptools.find_packages()
 )
