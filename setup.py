88b3723034f6b7ba43dbc0fc4d9f63fa02633140
elmarsan <mselias97@gmail.com>
2023-09-26 09:02:26 +0200

    copy setup.py in root

---
+++ setup.py
@@ -0,0 +1,13 @@
+setuptools.setup(
+    name="python_world",
+    version="0.0.1",
+    author="Elias Martinez",
+    author_email="mselias97@gmail.com",
+    description="A function that returns 'world'",
+    url="https://github.com/elmarsan/python_world_elmarsan",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
