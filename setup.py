58ed7b0e6d2bd95e1658840cdc8703127400fa21
Elías Martínez <mselias97@gmail.com>
2023-09-26 08:47:45 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,10 @@
-import setuptools
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="python_world",
     version="0.0.1",
     author="Elias Martinez",
     author_email="mselias97@gmail.com",
     description="A function that returns 'world'",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/elmarsan/python_world_elmarsan",
-    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
